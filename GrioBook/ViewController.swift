76cc717945f5151bb2fc2e53cba51593cb00b565
Eude Lesperance <l.keude96@gmail.com>
2016-01-25 16:01:29 -0600

    Initial Commit

---
+++ GrioBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrioBook
+//
+//  Created by Eude Kinsley Lesperance on 1/25/16.
+//  Copyright © 2016 Grio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
